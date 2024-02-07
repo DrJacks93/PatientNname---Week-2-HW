2094f3456d6c726696f0f6f9e1d15183980e8233
Derek <Drj2778@email.vccs.edu>
2024-02-06 22:43:25 -0500

    Add project files.

---
+++ PatientNname - Week 2 HW/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PatientNname___Week_2_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PatientNname");
+            Console.WriteLine("who is being seen today?");
+            String username = Console.ReadLine();
+            Console.WriteLine("welcome " + username);
+        }
+    }
+}
